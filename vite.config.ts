d7cd1b0bed18e879d72db6934c7a4c673d806b07
VRrelated <grainandglory01@gmail.com>
2025-05-16 19:48:45 +0530

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,11 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/truthcard_ai/",
+  build: {
+    rollupOptions: {
+      external: [
+        /@react-router\/dev\/routes/
+      ]
+    }
+  }
 });
